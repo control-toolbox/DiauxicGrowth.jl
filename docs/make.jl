88bcde70426a95c754c482ccf37ce50fb70d5041
ousmane-prog <ousmane-junior.sane@etu.univ-amu.fr>
2025-12-01 11:21:17 +0100

    Add implementation page and suppress documentation warnings

--- docs/make.jl
+++ docs/make.jl
@@ -29,13 +29,15 @@ makedocs(;
     format=Documenter.HTML(;
         repolink="https://" * repo_url,
         prettyurls=false,
-        size_threshold_ignore=["index.md"],
+        size_threshold_ignore=["index.md", "implementation.md"],
+        example_size_threshold = nothing,
         assets=[
             asset("https://control-toolbox.org/assets/css/documentation.css"),
             asset("https://control-toolbox.org/assets/js/documentation.js"),
         ],
     ),
-    pages=["Introduction" => "index.md"],
+    pages=["Introduction" => "index.md",
+    "Implementation and Results" => "implementation.md",],
 )
 
 deploydocs(; repo=repo_url * ".git", devbranch="main")
